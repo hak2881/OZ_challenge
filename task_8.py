eb58a251e639e02cb1829950b5f3e1225631a12a
hak1319 <gkr054@naver.com>
2024-11-26 15:44:56 +0900

    python 과제 7일차

---
+++ task_8.py
@@ -0,0 +1,11 @@
+import mymath
+
+
+
+print("삼각형의 넓이: {}".format(mymath.triangle_area(3, 10)))
+
+
+print("원의 넓이: {}".format(mymath.circle_area(4)))
+
+
+print("직육면체 넓이: {}".format(mymath.cuboid_area(5,10,4)))
